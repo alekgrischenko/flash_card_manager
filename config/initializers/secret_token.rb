ee4867532fc9c326a84c88accb9face803361f51
LelikGR <alek.grischenko@gmail.com>
2014-07-21 12:37:34 +0400

    Initial project version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlashCardMNGer::Application.config.secret_key_base = '8973d45926bdc09c20afd40ba3edf2edf553b0ed9c87e10ad2bd80e9791dddc1e96778047ee5d423a7ad3c8383f9fc58dd857f35b72d813da2924be93be3dc92'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlashCardMNGer::Application.config.session_store :cookie_store, key: '_flash_card_mаnаger_session'
