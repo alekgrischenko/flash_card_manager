f47ecb77c4e72e675c976f2485feddf1f155c973
LelikGR <alek.grischenko@gmail.com>
2014-07-23 17:20:46 +0400

    Create CRUD

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,5 +3,48 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
-  
+
+  def new
+    @card = Card.new
+  end
+   
+  def create
+    @card = Card.new(card_params)
+   
+    if @card.save
+      redirect_to @card
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @card = Card.find(params[:id])
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+
+    if @card.update(card_params)
+      redirect_to @card
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to cards_path    
+  end
+   
+  private
+    def card_params
+      params.require(:card).permit(:original_text, :translated_text, :review_date)
+    end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,5 @@
 class Card < ActiveRecord::Base
+
+  validates :original_text, :translated_text, presence: true 
+
 end
