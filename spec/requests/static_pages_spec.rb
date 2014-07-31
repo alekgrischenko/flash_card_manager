af54a7907252b5b2920b28701f85bb19268158d4
LelikGR <alek.grischenko@gmail.com>
2014-07-31 11:49:52 +0400

    Broken test with Capybara 2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,8 @@ describe "Static page" do
   it "does stuf" do
     FactoryGirl.create(:card, original_text: "bla bla")
     visit root_path
+    puts Card.first.id
     expect(page).to have_content "bla bla"
   end
-  
+
 end
