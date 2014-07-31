8d668f3b0cc86d248f9f1b54c350e49a5cc10197
LelikGR <alek.grischenko@gmail.com>
2014-07-31 13:32:59 +0400

    Right test with capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,9 +6,6 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 require 'database_cleaner'
 
-Capybara.javascript_driver = :poltergeist
-Capybara.default_driver = :poltergeist
-
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
@@ -19,15 +16,28 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.include Capybara::DSL
 
-  config.before(:suite) do
+  config.before :suite do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+ 
+  config.before type: :request do
+    DatabaseCleaner.strategy = :truncation
+  end
+ 
+  config.after type: :request  do
     DatabaseCleaner.strategy = :transaction
   end
-
-  config.before(:each) do
+ 
+  config.before do
     DatabaseCleaner.start
   end
-
-  config.after(:each) do
+ 
+  config.after do
     DatabaseCleaner.clean
   end
+
+  Capybara.javascript_driver = :poltergeist
+  Capybara.default_driver = :poltergeist
+
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,30 @@ require 'rails_helper'
 
 describe "Static page" do
   
-  it "does stuf" do
-    FactoryGirl.create(:card, original_text: "bla bla")
+  it "Home page have original word when rewiew date less than today" do
+    card = FactoryGirl.create(:card, original_text: "text", review_date:"30/07/2014")
     visit root_path
-    puts Card.first.id
-    expect(page).to have_content "bla bla"
+    expect(page).to have_content card.original_text
   end
 
+  it "Home page warning when rewiew date more than today" do
+    FactoryGirl.create(:card, review_date:"#{Time.now+1.day}")
+    visit root_path
+    expect(page).to have_content "Приходите завтра"
+  end
+
+  it "Message when translate wrong" do
+    FactoryGirl.create(:card)
+    visit root_path
+    click_button "Проверка"
+    expect(page).to have_content "Не правильно"
+  end
+
+  it "Message when translate right" do
+    FactoryGirl.create(:card)
+    visit root_path
+    fill_in 'translation', with: "текст"
+    click_button "Проверка"
+    expect(page).to have_content "Правильно"
+  end
 end
