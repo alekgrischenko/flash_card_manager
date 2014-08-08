e3b752f4cc6883d6f8f2161fc7da554b74c3ecf1
LelikGR <alek.grischenko@gmail.com>
2014-08-08 20:08:19 +0400

    Add ruby version in Gemfile

--- lib/aws.rb
+++ lib/aws.rb
@@ -3,6 +3,6 @@ module FCManagerAWS
   CONFIG_PATH = File.join(Rails.root, 'config/aws.yml')
 
   def self.config
-    @_config ||= YAML.load_file(CONFIG_PATH)[Rails.env]
+    @_config ||= YAML.load(ERB.new(File.read(CONFIG_PATH)).result)[Rails.env]
   end
 end
