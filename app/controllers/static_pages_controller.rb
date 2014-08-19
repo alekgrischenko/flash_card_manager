a5636bb5cae6f43e346a5aa8cf14f957350b6fbb
LelikGR <alek.grischenko@gmail.com>
2014-08-19 22:41:58 +0400

    Add method pending_cards to model user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,13 +2,6 @@ class StaticPagesController < ApplicationController
   skip_before_action :require_login, only: [:index]
 
   def index
-=begin
-    if current_user.try(:current_deck)
-      @card = current_user.current_deck.cards.pending.first 
-    elsif current_user
-      @card = current_user.cards.pending.first
-    end
-=end
     @card = current_user.pending_cards.first if current_user
   end
   
