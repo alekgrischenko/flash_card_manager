d37648fd3600ff86cd42dee22103d27e47c03fb5
LelikGR <alek.grischenko@gmail.com>
2014-08-20 22:46:15 +0400

    Corrected user validation error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 3 }, confirmation: true  
-  validates :password_confirmation, presence: true 
+  validates :password, length: { minimum: 3 }, confirmation: true, if: :password_set?  
+  validates :password_confirmation, presence: true, if: :password_set?
   validates :email, uniqueness: true 
 
   has_many :cards, dependent: :destroy
   has_many :decks, dependent: :destroy
   belongs_to :current_deck, class_name: "Deck", foreign_key: "current_deck_id"
+
+  def password_set?
+    @password
+  end
 
   def pending_cards
     if current_deck_id
