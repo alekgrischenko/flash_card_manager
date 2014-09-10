9b1e6b41c3446580b6175263bd39627cda5068c2
LelikGR <alek.grischenko@gmail.com>
2014-09-11 01:02:06 +0400

    Fix typo action

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,48 +1,28 @@
 class ReviewController < ApplicationController
 
   def check_translation
-    @card = Card.find(params[:card_id])
+    @card = current_user.cards.find(params[:card_id])
     reset_typo_count if !session[:typo_count]
     case @card.check(params[:translation]) 
     when :success
-      success_action
+      flash[:success] = "Верно"
+      reset_typo_count
+      redirect_to root_path
     when :typo
-      typo_action
+      if (session[:typo_count] < 3)
+        flash[:notice] = "Вы допустили опечатку. Количество оставшихся попыток: #{3 - session[:typo_count]}"
+        session[:typo_count] += 1
+        redirect_to root_path(card_id: @card.id)
+      else 
+        flash[:danger] = "Вы допустили опечатку более 3 раз"
+        reset_typo_count
+        redirect_to root_path
+      end
     else 
-      error_action
+      flash[:danger] = "Не правильно"
+      reset_typo_count
+      redirect_to root_path
     end
-  end
-
-  def success_action
-    flash[:success] = "Верно"
-    reset_typo_count
-    redirect_to root_path
-  end
-
-  def typo_action
-    if (session[:typo_count] < 3)
-      increment_typo_count
-    else 
-      to_many_typos
-    end
-  end
-
-  def error_action
-    flash[:danger] = "Не правильно"
-    reset_typo_count
-    redirect_to root_path
-  end
-
-  def increment_typo_count
-    flash[:notice] = "Вы допустили опечатку. Количество оставшихся попыток: #{3 - session[:typo_count]}"
-    session[:typo_count] += 1
-    redirect_to root_path(card_id: @card.id)
-  end
-
-  def to_many_typos
-    flash[:danger] = "Вы допустили опечатку более 3 раз"
-    reset_typo_count
-    redirect_to root_path
   end
 
   def reset_typo_count
