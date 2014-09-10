ad3f7b49d10e0335c879bf5354c1127dde930e1b
LelikGR <alek.grischenko@gmail.com>
2014-09-11 00:56:49 +0400

    Revert "Aborted change for refactoring method check_translation"
    This reverts commit 478613fbc8c59840a03b077fe0c62b1c774f6368.

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -2,24 +2,51 @@ class ReviewController < ApplicationController
 
   def check_translation
     @card = Card.find(params[:card_id])
+    reset_typo_count if !session[:typo_count]
     case @card.check(params[:translation]) 
     when :success
-      flash[:success] = "Верно"
-      redirect_to root_path
+      success_action
     when :typo
-      if session[:typo_count] < 3
-        flash[:notice] = "Вы допустили опечатку"
-        session[:typo_count] += 1
-        render "static_pages/index"
-      else 
-        flash[:danger] = "Вы допустили опечатку более 3 раз"
-        session[:typo_count] = 0
-        redirect_to root_path
-      end
+      typo_action
     else 
-      flash[:danger] = "Не правильно"
-      redirect_to root_path
+      error_action
     end
+  end
+
+  def success_action
+    flash[:success] = "Верно"
+    reset_typo_count
+    redirect_to root_path
+  end
+
+  def typo_action
+    if (session[:typo_count] < 3)
+      increment_typo_count
+    else 
+      to_many_typos
+    end
+  end
+
+  def error_action
+    flash[:danger] = "Не правильно"
+    reset_typo_count
+    redirect_to root_path
+  end
+
+  def increment_typo_count
+    flash[:notice] = "Вы допустили опечатку. Количество оставшихся попыток: #{3 - session[:typo_count]}"
+    session[:typo_count] += 1
+    redirect_to root_path(card_id: @card.id)
+  end
+
+  def to_many_typos
+    flash[:danger] = "Вы допустили опечатку более 3 раз"
+    reset_typo_count
+    redirect_to root_path
+  end
+
+  def reset_typo_count
+    session[:typo_count] = 0
   end
 
 end
