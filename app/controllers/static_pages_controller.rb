e95ca698c5e8bb992aa4c609b05489676193f3d6
LelikGR <alek.grischenko@gmail.com>
2014-09-10 22:59:36 +0400

    Refactoring method check_translation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,23 @@
 class StaticPagesController < ApplicationController
   skip_before_action :require_login, only: [:index]
+  before_action :current_user?
 
   def index
-    if current_user
-      if params[:card_id]
-        @card = Card.find(params[:card_id])
-      elsif current_user.cards.first
-        @card = current_user.pending_cards.first
-      elsif current_user.decks.empty?
-        redirect_to :new_deck, notice: 'Необходимо создать колоду и добавить туда карточки.'
-      else
-        redirect_to :decks, notice: 'Необходимо добавить карточки в колоду.'
-      end
+    if params[:card_id]
+      @card = current_user.cards.find(params[:card_id])
+    elsif current_user.cards.first
+      @card = current_user.pending_cards.first
+    elsif current_user.decks.empty?
+      redirect_to :new_deck, notice: 'Необходимо создать колоду и добавить туда карточки.'
+    else
+      redirect_to :decks, notice: 'Необходимо добавить карточки в колоду.'
+    end
+  end
+
+  def current_user?
+    if !current_user
+      flash[:notice] = "Войдите или зарегистрируйтесь"
+      redirect_to new_user_path
     end
   end
   
