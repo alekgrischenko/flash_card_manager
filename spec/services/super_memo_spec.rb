e4c57a9b6669bae3feeaef7c4bd3848e45348025
LelikGR <alek.grischenko@gmail.com>
2014-10-14 17:12:08 +0400

    Refactoring super_memo_spec.rb

--- spec/services/super_memo_spec.rb
+++ spec/services/super_memo_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "SuperMemo" do
+describe SuperMemo do
   
   before(:each) do
     @interval = 10
@@ -12,60 +12,69 @@ describe "SuperMemo" do
   end
 
   describe "calculate e-factor" do 
-    
+
     it "when time factor larger 0.5" do
       time = 15000
-      expect(SuperMemo.new(@interval, @e_factor, @typo_count, time, @translated_text_length, @numb_correct_answers).e_factor).to eq (@e_factor + 0.1 * @translated_text_length * 1000 / time)
+      supermemo = SuperMemo.new(@interval, @e_factor, @typo_count, time, @translated_text_length, @numb_correct_answers)
+      expect(supermemo.e_factor).to eq (@e_factor + 0.1 * @translated_text_length * 1000 / time)
     end
 
     it "when time factor less 0.5" do
       time = 30000
-      expect(SuperMemo.new(@interval, @e_factor, @typo_count, time, @translated_text_length, @numb_correct_answers).e_factor.round(2)).to eq @e_factor
+      supermemo = SuperMemo.new(@interval, @e_factor, @typo_count, time, @translated_text_length, @numb_correct_answers)
+      expect(supermemo.e_factor).to eq @e_factor
     end
 
     it "when typo count eq 1" do
       typo_count = 1
-      expect(SuperMemo.new(@interval, @e_factor, typo_count, @time, @translated_text_length, @numb_correct_answers).e_factor.round(2)).to eq (@e_factor - 0.14 * @translated_text_length * 1000 / @time)
+      supermemo = SuperMemo.new(@interval, @e_factor, typo_count, @time, @translated_text_length, @numb_correct_answers)
+      expect(supermemo.e_factor).to eq (@e_factor - 0.14 * @translated_text_length * 1000 / @time)
     end
   end
 
   describe "calculate interval" do
-    describe "typo count <= 1" do
-      it "number correct answers eq 1" do
+    describe "when typo count less or equal than 1" do
+      it "and number correct answers eq 1" do
         numb_correct_answers = 1
-        expect(SuperMemo.new(@interval, @e_factor, @typo_count, @time, @translated_text_length, numb_correct_answers).interval).to eq 1
+        supermemo = SuperMemo.new(@interval, @e_factor, @typo_count, @time, @translated_text_length, numb_correct_answers)
+        expect(supermemo.interval).to eq 1
       end
 
-      it "number correct answers eq 2" do
+      it "and number correct answers eq 2" do
         numb_correct_answers = 2
-        expect(SuperMemo.new(@interval, @e_factor, @typo_count, @time, @translated_text_length, numb_correct_answers).interval).to eq 6
+        supermemo = SuperMemo.new(@interval, @e_factor, @typo_count, @time, @translated_text_length, numb_correct_answers)
+        expect(supermemo.interval).to eq 6
       end
 
-      describe "number correct answers larger 2" do
+      describe "and number correct answers larger 2" do
         it "and interval * e_factor < 365" do
           numb_correct_answers = 3
-          expect(SuperMemo.new(@interval, @e_factor, @typo_count, @time, @translated_text_length, numb_correct_answers).interval).to eq @interval * @e_factor
+           supermemo = SuperMemo.new(@interval, @e_factor, @typo_count, @time, @translated_text_length, numb_correct_answers)
+          expect(supermemo.interval).to eq @interval * @e_factor
         end
 
-        it "and interval * e_factor >= 365" do
+        it "and interval * e_factor larger or equal than 365" do
           numb_correct_answers = 3
           interval = 150
-          expect(SuperMemo.new(interval, @e_factor, @typo_count, @time, @translated_text_length, numb_correct_answers).interval).to eq 365
+          supermemo = SuperMemo.new(interval, @e_factor, @typo_count, @time, @translated_text_length, numb_correct_answers)
+          expect(supermemo.interval).to eq 365
         end
       end
     end
 
-    describe "typo_count > 1" do
+    describe "when typo count larger 1" do
 
       before(:each) { @typo_count = 2 }
 
       it "and interval larger e-factor" do
-        expect(SuperMemo.new(@interval, @e_factor, @typo_count, @time, @translated_text_length, @numb_correct_answers).interval).to eq @interval / @e_factor
+        supermemo = SuperMemo.new(@interval, @e_factor, @typo_count, @time, @translated_text_length, @numb_correct_answers)
+        expect(supermemo.interval).to eq @interval / @e_factor
       end
 
-      it "and interval <= e-factor " do
+      it "and interval less or equal than e-factor " do
         interval = 2
-        expect(SuperMemo.new(interval, @e_factor, @typo_count, @time, @translated_text_length, @numb_correct_answers).interval).to eq 1
+        supermemo = SuperMemo.new(interval, @e_factor, @typo_count, @time, @translated_text_length, @numb_correct_answers)
+        expect(supermemo.interval).to eq 1
       end
     end
   end
