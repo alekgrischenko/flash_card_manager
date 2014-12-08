49dda6927d6c52c897850de5064ed86623d794fe
LelikGR <alek.grischenko@gmail.com>
2014-12-08 23:56:22 +0300

    Add dotenv for enviroment variables - delete redundant files

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
-require 'dotenv' #'aws'
+require 'dotenv'
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'], #FCManagerAWS.config['aws_access_key_id'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], #FCManagerAWS.config['aws_secret_access_key'],
-    region:                ENV['REGION'], #FCManagerAWS.config['region'],
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region:                ENV['REGION'],
     path_style: true,
   }
-  config.fog_directory  = ENV['BUCKET_NAME'] #FCManagerAWS.config['bucket_name']
+  config.fog_directory  = ENV['BUCKET_NAME']
 end
